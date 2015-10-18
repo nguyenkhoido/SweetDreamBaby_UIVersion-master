bf71f76679005b42ca794808c48d9ebfc0b560f9
thinhpdps02415 <thinhpdps02415@fpt.edu.vn>
2015-10-18 09:57:24 +0700

    Thinh

--- app/src/main/java/com/SweetDream/Activity/PlayingPage.java
+++ app/src/main/java/com/SweetDream/Activity/PlayingPage.java
@@ -2,24 +2,43 @@ package com.SweetDream.Activity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.SweetDream.Audio.SongsManager;
+import com.SweetDream.Audio.Utilities;
 import com.SweetDream.R;
 import com.parse.GetCallback;
 import com.parse.ParseException;
-import com.parse.ParseFile;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by nguye_000 on 07/10/2015.
  */
 public class PlayingPage extends AppCompatActivity {
-    ImageButton btnBackActivity,btnRandom,btnBack,btnPrevious,btnPause,btnPlay,btnForward,btnNext,btnLoop;
-    //Button btnTimes;
+   private ImageButton btnBackActivity,btnRandom,btnBack,btnPrevious,btnPause,btnPlay,btnForward,btnNext,btnLoop;
+    private SeekBar songProgressBar;
+    private TextView songTitleLabel ,songCurrentDurationLabel,songTotalDurationLabel;
+    private MediaPlayer mp;
+    private Handler mHandler = new Handler();;
+    private SongsManager songManager;
+    private Utilities utils;
+    private int seekForwardTime = 5000; // 5000 milliseconds
+    private int seekBackwardTime = 5000; // 5000 milliseconds
+    private int currentSongIndex = 0;
+    private boolean isShuffle = false;
+    private boolean isRepeat = false;
+    private ArrayList<HashMap<String, String>> songsList = new ArrayList<HashMap<String, String>>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
